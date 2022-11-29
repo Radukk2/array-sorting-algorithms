d8ad362f5e891f49ef0e93a5fad869a5b3048cb9
Student USO VM User <student@stud.acs.upb.ro>
2022-11-29 17:07:34 +0200

    radix-sort.c

---
+++ radix-sort.c
@@ -0,0 +1,14 @@
+void radixsort(int arr[], int n)
+{
+    // Find the maximum number to know number of digits
+    int m = getMax(arr, n);
+ 
+    // Do counting sort for every digit. Note that instead
+    // of passing digit number, exp is passed. exp is 10^i
+    // where i is current digit number
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+}
+ 
+// A utility function to print an array
+
